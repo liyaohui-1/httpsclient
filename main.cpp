c6ec70b43863311b5269587c9072ef9494db8755
yaohui.li <yaohui.li@invo.cn>
2024-12-25 21:43:55 +0800

    feat:add nlohmann

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <memory>
 #include "https_client.h"
 #include "zlib.h"
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
 
 int main()
 {
@@ -26,10 +29,31 @@ int main()
     {
         std::cout << "client.Init() Successed!" << std::endl;
     }
- 
-    std::string data= {"{ { \"timestamp\":1647253105799, \"hardware_version\":\"A.1\", \"sottware_version\":\"S.181020.V5.22.R\", \"account_id\":\"0M300OCAVR183\", \"device_id\":\"GAC5846515\", \"device_brand\":\"德赛\", \"device_os_name\":\"A06AVNT\", \"platform\":\"android5\", \"body\":[ { \"tag\":3, \"service_id\":\"0x10C0\", \"service_interface_id\":\"0x0001\", \"fault_time_stamp\":1647253105799, \"fault_code\":51380242, \"fault_string\":\"大数据预处理文件压缩故障\", \"fault_reason\":\"unable to locate the component\", \"fault_detail\":\"\" } ] } }" };
+    
+    json j; // 首先创建一个空的json对象
+    j["timestamp"] = 1647253105799;
+    j["hardware_version"] = "A.1";
+    j["sottware_version"] = "S.181020.V5.22.R";
+    j["account_id"] = "0M300OCAVR183";
+    j["device_id"] = "GAC5846515";
+    j["device_brand"] = "德赛";
+    j["device_os_name"] = "A06AVNT";
+    j["platform"] = "android5";
+
+    j["body"][0]["tag"] = 3;
+    j["body"][0]["service_id"] = "0x10C0";
+    j["body"][0]["service_interface_id"] = "0x0001";
+    j["body"][0]["fault_time_stamp"] = 1647253105799;
+    j["body"][0]["fault_code"] = 51380242;
+    j["body"][0]["fault_string"] = "大数据预处理文件压缩故障";
+    j["body"][0]["fault_reason"] = "unable to locate the component";
+    j["body"][0]["fault_detail"] = "";
+
+    std::string data = j.dump();
     std::string destChar;
     unsigned long nread,nwrite;
+
+    // std::string data= {"{ { \"timestamp\":1647253105799, \"hardware_version\":\"A.1\", \"sottware_version\":\"S.181020.V5.22.R\", \"account_id\":\"0M300OCAVR183\", \"device_id\":\"GAC5846515\", \"device_brand\":\"德赛\", \"device_os_name\":\"A06AVNT\", \"platform\":\"android5\", \"body\":[ { \"tag\":3, \"service_id\":\"0x10C0\", \"service_interface_id\":\"0x0001\", \"fault_time_stamp\":1647253105799, \"fault_code\":51380242, \"fault_string\":\"大数据预处理文件压缩故障\", \"fault_reason\":\"unable to locate the component\", \"fault_detail\":\"\" } ] } }" };
 
     nread = data.length();
     destChar.resize(nread);
